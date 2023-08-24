3282d0c95a3ac2773368c368131a31dffc5ee3c8
MaksElena <MaksElenaIT@yandex.ru>
2023-08-24 18:20:18 +0300

    Возврат настроек дебуга.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,19 +1,14 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-user24082023')
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ' ').split(',')
+ALLOWED_HOSTS = []
 
 
 # Application definition
